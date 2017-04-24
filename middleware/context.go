021444eaca90755c52bc34191157bf2151570206
Nicholas Law <nick@nicholaslaw.com.au>
2017-04-24 11:10:29 +1000

    Drop unused CustomContext

--- middleware/context.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "github.com/labstack/echo"
-
-// CustomContext is a custom context that extends Echo's
-// base Context implementation
-type CustomContext struct {
-	echo.Context
-}
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -7,12 +7,6 @@ import (
 
 // RegisterMiddleware registers all required middleware
 func RegisterMiddleware(svr *echo.Echo) {
-	svr.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			cc := &CustomContext{c}
-			return h(cc)
-		}
-	})
 	svr.Pre(middleware.RemoveTrailingSlash())
 	svr.Use(middleware.Logger())
 	svr.Use(middleware.Recover())
